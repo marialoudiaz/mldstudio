c66c52735d1fb31ef3dc05b8c541ade774b515cf
marialoudiaz <loumariadiaz@gmail.com>
2023-11-25 01:01:28 +0100

    contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -27,6 +27,9 @@ const Contact =()=>{
             <li>
               <textarea placeholder='Message' name='message' required></textarea>
             </li>
+            <li>
+              <input type='submit' className='flat-button' value='SEND'/>
+            </li>
            </ul>
         </form>
       </div>
@@ -38,3 +41,33 @@ const Contact =()=>{
 
   }
 export default Contact
+
+// import React, { useRef } from 'react';
+// import emailjs from '@emailjs/browser';
+
+// export const ContactUs = () => {
+//   const form = useRef();
+
+//   const sendEmail = (e) => {
+//     e.preventDefault();
+
+//     emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+//       .then((result) => {
+//           console.log(result.text);
+//       }, (error) => {
+//           console.log(error.text);
+//       });
+//   };
+
+//   return (
+//     <form ref={form} onSubmit={sendEmail}>
+//       <label>Name</label>
+//       <input type="text" name="user_name" />
+//       <label>Email</label>
+//       <input type="email" name="user_email" />
+//       <label>Message</label>
+//       <textarea name="message" />
+//       <input type="submit" value="Send" />
+//     </form>
+//   );
+// };
