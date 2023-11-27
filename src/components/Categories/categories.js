4875079ffddf3a49108a9e5fac4ccd995b248430
marialoudiaz <loumariadiaz@gmail.com>
2023-11-27 14:01:57 +0100

    affichage dynamique projet

--- src/components/Categories/categories.js
+++ src/components/Categories/categories.js
@@ -3,7 +3,25 @@ import './categories.scss';
 
 function index() {
   return (
-    <div>catégories</div>
+    <div className='grid-column'>
+      <div>
+        <img />
+        <p>Identité visuelle</p>
+      </div>
+      <div>
+        <img />
+        <p>Développement web et mobile</p>
+      </div>
+      <div>
+        <img />
+        <p>Illustration</p>
+      </div>
+      <div>
+        <img />
+        <p>Typographie</p>
+      </div>
+      
+    </div>
   )
 }
 
